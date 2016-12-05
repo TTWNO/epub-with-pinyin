6221dffe85c1809c0493c3fe287fbeefa5dd4edb
Tait Hoyem <taitskywalker@gmail.com>
2016-12-05 12:52:38 -0700

    Fix bug with links, causing invalid XML

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #!/bin/python3.4
 # -*- coding: utf-8 -*-
+"""A Python app that makes epub files with pinyin in them."""
+
+import sys
+import html as htmlLib
+import os
+
 from dragonmapper import transcriptions as trans
 from dragonmapper import hanzi
 from dragonmapper import html
 import hanzidentifier as ziid
-from bs4 import BeautifulSoup
 from ebooklib import epub
 import jieba
+
+from bs4 import BeautifulSoup
 import bs4
-import sys
-import os
 
 # Change These #
 debug_is_on = False
@@ -26,11 +31,17 @@ NEW_DIR = FILE_NAME + '-with-pinyin'
 EPUB_BLACKLIST_KEYWORDS = ['pagenav']
 
 GET_TAG_NAMES = ['p', 'strong', 'h1',
-                    'h2', 'h3', 'h4',
-                    'h5', 'h6', 'i',
-                    'b', 'u', 'a']
+                 'h2', 'h3', 'h4',
+                 'h5', 'h6', 'i',
+                 'b', 'u', 'a', 'em']
+
 
 def is_entirely_chinese(s):
+    """
+    Check if every character in the string is Chinese.
+
+    *s* string to search in
+    """
     debug('TEST(IEC): '+str(s))
     for c in s:
         if not ziid.has_chinese(c):
@@ -38,19 +49,36 @@ def is_entirely_chinese(s):
             return False
     return True
 
+
 def debug(s):
+    """
+    Print info to screen, but only when debugging is enabled.
+
+    *s* object to print
+    """
     if debug_is_on:
         print(s)
 
 
 def find(name, path):
-    for root, dirs, files in os.walk(path):
-        debug('{1}/{2}'.format(root, path, name))
-        if name in files:
+    """
+    Look for, and return (if found) the path of a file.
+
+    *name* name of the file
+    *path* directory to look in
+    """
+    for root, drs, fls in os.walk(path):
+        debug('ROOT: {0}\nDIRS: {1}\nFILES: {2}'.format(root, drs, fls))
+        if name in fls:
             return os.path.join(root, name)
 
 
 def find_epub_files(f):
+    """
+    Get list of files needed to be converted.
+
+    *f* epub file no extention
+    """
     all_star_html = []
     book = epub.read_epub(f + '.epub')
     for t in book.toc:
@@ -70,10 +98,10 @@ def find_epub_files(f):
         addExtrFile = find(extr_file, f)
         debug(addStnFile)
         debug(addExtrFile)
-        if addStnFile != None:
+        if addStnFile is not None:
             debug("Adding file")
             all_star_html.append(addStnFile)
-        if addExtrFile != None:
+        if addExtrFile is not None:
             debug("Adding -extracted file")
             all_star_html.append(addExtrFile)
     return all_star_html
@@ -81,9 +109,9 @@ def find_epub_files(f):
 
 def get_all_tags_text(pt):
     """
-    *pt* parent tag
+    Get all text from inner tags.
 
-    Get all text from inner tags
+    *pt* parent tag
     """
     all_tags_text = []
     for tag in pt.contents:
@@ -92,11 +120,41 @@ def get_all_tags_text(pt):
     return all_tags_text
 
 
-def get_pinyin_bs(fn):
+def generate_new_html_for_words(words):
+    """
+    Go through list of words, and generates the HTML + Ruby characters.
 
+    *words* a list of chinese words
+    """
+    words_with_pinyin_html = ""
+
+    for word in words:
+        debug("{0} : {1}".format(word, "".join(words)))
+        debug('CHARACTER: {0}'.format(word))
+        if not is_entirely_chinese(word):
+            pi = (' '*len(word)).split(' ')
+        else:
+            zh = hanzi.to_zhuyin(word)
+            debug('ZHUYIN: {0}'.format(zh))
+            pi = trans.zhuyin_to_pinyin(
+                zh,
+                accented=not numbered_pinyin).split(' ')
+            debug('PINYIN: {0}'.format(pi))
+        debug("{0}={1}".format(word, "".join(pi)))
+        words_with_pinyin_html += html.to_html(word, top=pi, minified=True)
+
+    return words_with_pinyin_html
+
+
+def get_pinyin_bs(file_name):
+    """
+    Return file + Ruby characters.
+
+    *fn* file name to run on
+    """
     # open file and parse with bs4, with xml rules
-    debug(fn)
-    f = open(fn, 'r')
+    debug(file_name)
+    f = open(file_name, 'r')
     s1 = f.read()
     f.close()
     bs = BeautifulSoup(s1, 'xml')
@@ -107,6 +165,8 @@ def get_pinyin_bs(fn):
 
     for tn in GET_TAG_NAMES:
         for t in bs.findAll(tn):
+            if "href" in t:
+                t['href'] = htmlLib.escape(t['href'])
             for ts in get_all_tags_text(t):
                 all_of_the_tags.append(ts)
 
@@ -116,25 +176,15 @@ def get_pinyin_bs(fn):
         p_text = p['str']
         debug(p_text)
         new_p_str = ''
+
         debug('WORKING ON: ')
         debug("#######{0}#######".format(p_text))
         # pos_tagging will give you the type of word
         # unneccesary in this case
         words = [word for word in jieba.cut(p_text)]
         debug("WORDS: {0}".format(words))
-        for word in words:
-            debug("{0} : {1}".format(word, str(p_el)))
-            debug('CHARACTER: {0}'.format(word))
-            if not is_entirely_chinese(word):
-                pi = (' '*len(word)).split(' ')
-            else:
-                zh = hanzi.to_zhuyin(word)
-                debug('ZHUYIN: {0}'.format(zh))
-                pi = trans.zhuyin_to_pinyin(zh, accented=not(numbered_pinyin)).split(' ')
-                debug('PINYIN: {0}'.format(pi))
-            debug("{0}={1}".format(word, "".join(pi)))
-            new_p_str += html.to_html(word, top=pi, minified=True)
-        debug("P_EL: " + str(p_el.parent))
+        new_p_str = generate_new_html_for_words(words)
+        debug("Parent Element: " + str(p_el.parent))
         debug("{0} will be replaced by {1}".format(p_el, new_p_str))
         p_el.replace_with(bs.new_string(new_p_str))
     return str(bs.decode(formatter=None))
@@ -150,7 +200,10 @@ for fn in files:
 
 for fn in files:
     if fn != "":
-        print("{0}({1})/{2} starting... ".format(current_file_num, fn, total_files))
+        print("{0}({1})/{2} starting... ".format(
+            current_file_num,
+            fn,
+            total_files))
         new_text = get_pinyin_bs(fn)
         debug(new_text)
         new_f = open("{0}/{1}".format(NEW_DIR, fn), encoding='utf-8', mode='w')
