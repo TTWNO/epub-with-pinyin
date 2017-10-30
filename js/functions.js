4d138ef4b82fc6c3c1a635bf14af4ad3046b294f
Tait Hoyem <taitskywalker@gmail.com>
2017-10-30 20:47:02 +0000

    Add spacing between words with pinyin, remove space when word is known

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,25 @@
-let remove_words_css = document.createElement("style");
-function run_on_word_click(e) {
-    let clickedWord = e.classList[0];
-    console.log(clickedWord);
+let remove_words_css = document.createElement("style"), base_css = "ruby{padding-left:0.1em;padding-right:0.1em;}";
+function generate_css_to_remove_pinyin(word) {
+    return "rt." + word + ".phonetic-script{display:none;}ruby." + word + "{padding:0em;}";
 }
-function remove_pinyin_css(word) {
-    remove_words_css.innerHTML += "." + word + ".phonetic-script{visibility:hidden;}";
+function run_on_word_click(mouseClick) {
+    let clickedWord = mouseClick.toElement.classList[0];
+    reverse_has_pinyin(clickedWord);
+}
+function reverse_has_pinyin(word) {
+    let removeCSS = generate_css_to_remove_pinyin(word);
+    let foundAt = remove_words_css.innerHTML.search(removeCSS);
+    if (foundAt == -1) {
+        remove_words_css.innerHTML += removeCSS;
+    }
+    else {
+        remove_words_css.innerHTML = remove_words_css.innerHTML.replace(removeCSS, "");
+    }
 }
 function add_css_style_for_words() {
     remove_words_css['id'] = "hide_words";
+    remove_words_css.innerHTML += base_css;
     document.head.appendChild(remove_words_css);
 }
 add_css_style_for_words();
+document.addEventListener('click', run_on_word_click);
--- js/functions.ts
+++ js/functions.ts
@@ -1,18 +1,31 @@
-let remove_words_css: HTMLElement = document.createElement("style");
+let remove_words_css: HTMLElement = document.createElement("style"),
+    base_css: string = "ruby{padding-left:0.1em;padding-right:0.1em;}";
 
-function run_on_word_click(e: HTMLElement){
-    let clickedWord: String = e.classList[0];
-    console.log(clickedWord);
+function generate_css_to_remove_pinyin(word: string){
+    return "rt."+word+".phonetic-script{display:none;}ruby."+word+"{padding:0em;}";
 }
 
-function remove_pinyin_css(word: string){
-    remove_words_css.innerHTML += "."+word+".phonetic-script{visibility:hidden;}";
+function run_on_word_click(mouseClick: MouseEvent){
+    let clickedWord: string = mouseClick.toElement.classList[0];
+    reverse_has_pinyin(clickedWord);
+}
+
+function reverse_has_pinyin(word: string){
+    let removeCSS: string = generate_css_to_remove_pinyin(word);
+    let foundAt: number = remove_words_css.innerHTML.search(removeCSS);
+    if (foundAt == -1){
+        remove_words_css.innerHTML += removeCSS;
+    } else {
+        remove_words_css.innerHTML = remove_words_css.innerHTML.replace(removeCSS, "");
+    }
 }
 
 function add_css_style_for_words(){
     remove_words_css['id'] = "hide_words";
+    remove_words_css.innerHTML += base_css;
     document.head.appendChild(
         remove_words_css
     );
 }
 add_css_style_for_words();
+document.addEventListener('click', run_on_word_click);
